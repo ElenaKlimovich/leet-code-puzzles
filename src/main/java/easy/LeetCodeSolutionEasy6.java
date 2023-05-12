ec746d45dbaa49e527000a1c445b95e1d5bb175a
elenaklimovich <eklimovich@systematica-consulting.ru>
2023-05-12 12:47:59 +0300

    merge 2 sorted linked Lists

--- src/main/java/easy/LeetCodeSolutionEasy6.java
+++ src/main/java/easy/LeetCodeSolutionEasy6.java
@@ -1,13 +1,24 @@
 package easy;
 
-import java.util.*;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
 
 public class LeetCodeSolutionEasy6 {
 
     public static void main(String[] args) {
 //        System.out.println(Arrays.toString(twoSum(new int[]{2, 7, 11, 15}, 9))); // [0,1]
-        System.out.println(isValid("()[]{}")); // true
+//        System.out.println(isValid("()[]{}")); // true
 
+        ListNode list14 = new ListNode(4);
+        ListNode list12 = new ListNode(2, list14);
+        ListNode list1 = new ListNode(1, list12);
+
+        ListNode list24 = new ListNode(4);
+        ListNode list23 = new ListNode(3, list24);
+        ListNode list2 = new ListNode(1, list23);
+        ListNode node = mergeTwoLists(list1, list2); // //list1 = [1,2,4], list2 = [1,3,4]  Output: [1,1,2,3,4,4]
     }
 
     //  1.  https://leetcode.com/problems/two-sum
@@ -54,7 +65,44 @@ public class LeetCodeSolutionEasy6 {
     }
 
 
-//    https://leetcode.com/problems/merge-two-sorted-lists
+    //  3.  https://leetcode.com/problems/merge-two-sorted-lists
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode result = new ListNode(Integer.MIN_VALUE);
+        ListNode pointer = result;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                pointer.next = list1;
+                list1 = list1.next;
+            } else {
+                pointer.next = list2;
+                list2 = list2.next;
+            }
+            pointer = pointer.next;
+        }
+
+        pointer.next = list1 == null ? list2 : list1;
+
+        return result.next;
+    }
+
 //    https://leetcode.com/problems/implement-strstr
 //    https://leetcode.com/problems/merge-sorted-array
 //    https://leetcode.com/problems/symmetric-tree
