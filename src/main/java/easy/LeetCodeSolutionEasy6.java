63edfd8f867962fc315e7799deccccafe1d31e31
elenaklimovich <eklimovich@systematica-consulting.ru>
2023-05-14 12:04:02 +0300

    merge 2 sorted arrays

--- src/main/java/easy/LeetCodeSolutionEasy6.java
+++ src/main/java/easy/LeetCodeSolutionEasy6.java
@@ -1,9 +1,6 @@
 package easy;
 
-import java.util.Deque;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
+import java.util.*;
 
 public class LeetCodeSolutionEasy6 {
 
@@ -11,14 +8,21 @@ public class LeetCodeSolutionEasy6 {
 //        System.out.println(Arrays.toString(twoSum(new int[]{2, 7, 11, 15}, 9))); // [0,1]
 //        System.out.println(isValid("()[]{}")); // true
 
-        ListNode list14 = new ListNode(4);
-        ListNode list12 = new ListNode(2, list14);
-        ListNode list1 = new ListNode(1, list12);
+//        ListNode list14 = new ListNode(4);
+//        ListNode list12 = new ListNode(2, list14);
+//        ListNode list1 = new ListNode(1, list12);
+//
+//        ListNode list24 = new ListNode(4);
+//        ListNode list23 = new ListNode(3, list24);
+//        ListNode list2 = new ListNode(1, list23);
+//        ListNode node = mergeTwoLists(list1, list2); // //list1 = [1,2,4], list2 = [1,3,4]  Output: [1,1,2,3,4,4]
 
-        ListNode list24 = new ListNode(4);
-        ListNode list23 = new ListNode(3, list24);
-        ListNode list2 = new ListNode(1, list23);
-        ListNode node = mergeTwoLists(list1, list2); // //list1 = [1,2,4], list2 = [1,3,4]  Output: [1,1,2,3,4,4]
+//        System.out.println(strStr("sadbutsad", "sad")); // 0
+
+//        merge(new int[]{1, 3, 5, 0, 0, 0}, 3, new int[]{2, 2, 6}, 3); // [1,2,2,3,5,6]
+//        merge(new int[]{4, 5, 6, 7, 0, 0, 0}, 4, new int[]{1, 2, 3}, 3);
+//        merge(new int[]{0}, 1, new int[]{1}, 1);
+        merge(new int[]{1, 2, 3, 0, 0, 0}, 3, new int[]{2, 5, 6}, 3);
     }
 
     //  1.  https://leetcode.com/problems/two-sum
@@ -103,8 +107,48 @@ public class LeetCodeSolutionEasy6 {
         return result.next;
     }
 
-//    https://leetcode.com/problems/implement-strstr
-//    https://leetcode.com/problems/merge-sorted-array
+    //   4. https://leetcode.com/problems/implement-strstr
+    public static int strStr(String haystack, String needle) {
+//        return haystack.indexOf(needle);
+
+
+        int lh = haystack.length();
+        int ln = needle.length();
+
+        if (lh < ln)
+            return -1;
+
+        for (int i = 0; i < lh - ln; i++) {
+            int j = 0;
+
+            while (j < ln && haystack.charAt(i + j) == needle.charAt(j)) {
+                j++;
+            }
+            if (j == ln)
+                return i;
+        }
+
+        return -1;
+    }
+
+    //   5. https://leetcode.com/problems/merge-sorted-array
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        int i = m - 1, j = n - 1, l = m + n - 1;
+        while (j >= 0) {
+            if (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[l] = nums1[i];
+                i--;
+            } else {
+                nums1[l] = nums2[j];
+                j--;
+            }
+            l--;
+        }
+
+        System.out.println(Arrays.toString(nums1));
+    }
+
 //    https://leetcode.com/problems/symmetric-tree
 //    https://leetcode.com/problems/valid-palindrome
 //    https://leetcode.com/problems/count-primes
