a1538f116d42738532ee968462532c515f96c889
elenaklimovich <eklimovich@systematica-consulting.ru>
2023-05-15 18:20:57 +0300

    swapping nodes in a linked list

--- src/main/java/medium/LeetCodeSolutionMedium6.java
+++ src/main/java/medium/LeetCodeSolutionMedium6.java
@@ -3,10 +3,57 @@ package medium;
 public class LeetCodeSolutionMedium6 {
 
     public static void main(String[] args) {
+        ListNode node5 = new ListNode(5);
+        ListNode node4 = new ListNode(4, node5);
+        ListNode node3 = new ListNode(3, node4);
+        ListNode node2 = new ListNode(2, node3);
+        ListNode node = new ListNode(1, node2);
 
+        swapNodes(node, 2); //[1,2,3,4,5] -> [1,4,3,2,5]
     }
 
-//    public static List<List<String>> partition(String s) {
-//
-//    }
+//    https://leetcode.com/problems/swapping-nodes-in-a-linked-list/
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static ListNode swapNodes(ListNode head, int k) {
+
+        ListNode node1 = head;
+
+        while (k>1) {
+            node1 = node1.next;
+            k--;
+        }
+
+        ListNode slowPointer = head;
+        ListNode fastPointer = node1;
+
+        while (fastPointer.next != null) {
+            fastPointer = fastPointer.next;
+            slowPointer = slowPointer.next;
+        }
+
+        ListNode node2 = slowPointer;
+
+        int temp = node1.val;
+        node1.val = node2.val;
+        node2.val = temp;
+
+        return head;
+    }
 }
