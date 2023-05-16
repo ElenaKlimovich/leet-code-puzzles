3c4a1ef15e1f58cc90b2083da3b67edf1388a01c
elenaklimovich <eklimovich@systematica-consulting.ru>
2023-05-16 14:05:51 +0300

    swap nodes in pairs

--- src/main/java/medium/LeetCodeSolutionMedium6.java
+++ src/main/java/medium/LeetCodeSolutionMedium6.java
@@ -3,17 +3,51 @@ package medium;
 public class LeetCodeSolutionMedium6 {
 
     public static void main(String[] args) {
-        ListNode node5 = new ListNode(5);
-        ListNode node4 = new ListNode(4, node5);
+//        ListNode node5 = new ListNode(5);
+//        ListNode node4 = new ListNode(4, node5);
+//        ListNode node3 = new ListNode(3, node4);
+//        ListNode node2 = new ListNode(2, node3);
+//        ListNode node = new ListNode(1, node2);
+//        swapNodes(node, 2); //[1,2,3,4,5] -> [1,4,3,2,5]
+
+        ListNode node4 = new ListNode(4);
         ListNode node3 = new ListNode(3, node4);
         ListNode node2 = new ListNode(2, node3);
         ListNode node = new ListNode(1, node2);
-
-        swapNodes(node, 2); //[1,2,3,4,5] -> [1,4,3,2,5]
+        swapPairs(node); //[1,2,3,4] -> [2,1,4,3]
     }
 
-//    https://leetcode.com/problems/swapping-nodes-in-a-linked-list/
+    // https://leetcode.com/problems/swap-nodes-in-pairs/
+    public static ListNode swapPairs(ListNode head) {
 
+        if (head == null || head.next == null)
+            return head;
+
+        ListNode dummyNode = new ListNode();
+        dummyNode.next = head;
+        ListNode pointer = dummyNode;
+
+        while (pointer != null) {
+            ListNode node1 = pointer.next;
+            ListNode node2 = null;
+
+            if (node1 != null)
+                node2 = node1.next;
+
+            if (node2 != null) {
+                ListNode nextNode2 = node2.next;
+                node2.next = node1;
+                pointer.next = node2;
+                node1.next = nextNode2;
+                pointer = node1;
+            } else
+                break;
+        }
+
+        return dummyNode.next;
+    }
+
+    // https://leetcode.com/problems/swapping-nodes-in-a-linked-list/
     private static class ListNode {
         int val;
         ListNode next;
@@ -35,7 +69,7 @@ public class LeetCodeSolutionMedium6 {
 
         ListNode node1 = head;
 
-        while (k>1) {
+        while (k > 1) {
             node1 = node1.next;
             k--;
         }
