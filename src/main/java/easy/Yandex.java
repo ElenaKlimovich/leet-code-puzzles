28fa5c39900dddd9ed7d3134bf85e537cc91c696
elenaklimovich <eklimovich@systematica-consulting.ru>
2024-07-01 14:05:25 +0300

    test rbs 3 imports

--- src/main/java/easy/Yandex.java
+++ src/main/java/easy/Yandex.java
@@ -1,6 +1,12 @@
 package easy;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
 
 public class Yandex {
 
